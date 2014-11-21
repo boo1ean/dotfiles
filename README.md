7773bd4b629e1a0e7198736dc47a7ded3afb8c08
boo1ean <boo1ean0807@gmail.com>
2014-11-21 14:17:15 +0200

    Add quick setup script

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-## Some common configs for my environment ##
+## Some common configs for my environment
 
 * Shell - [bash](http://en.wikipedia.org/wiki/Bash_\(Unix_shell)
 * Window manager - [xmonad](http://xmonad.org/)
 * Text editor - [vim](http://www.vim.org/)
+
+## Installation
+
+```
+curl -sSL https://raw.githubusercontent.com/boo1ean/dotfiles/master/setup.sh | sudo bash
+```
---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# setup dotdeb repos
+#
+# wget http://www.dotdeb.org/dotdeb.gpg
+# apt-key add dotdeb.gpg
+# rm dotdeb.gpg
+# 
+# echo "deb http://packages.dotdeb.org wheezy all" | tee -a /etc/apt/sources.list.d/dotdeb.list
+# echo "deb-src http://packages.dotdeb.org wheezy all" | tee -a /etc/apt/sources.list.d/dotdeb.list
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# install system-wide stuff
+apt-get update
+apt-get install -y \
+	tmux \
+	vim \
+	git \
+	nginx-full \
+	postgresql-server-dev-9.3
+
+# install configs
+git clone https://github.com/boo1ean/dotfiles.git
+mv dotfiles/.aliases   ~/
+mv dotfiles/.bashrc    ~/
+mv dotfiles/.gitconfig ~/
+mv dotfiles/.tmux.conf ~/
+mv dotfiles/.vimrc     ~/
+mv dotfiles/.zshrc     ~/
+mv dotfiles/.vim       ~/
+
+rm -rf dotfiles
+
+vim +BundleInstall +qall

a15cb3a736c154ad8a6e01f8226a738b2f3d6b87
boo1ean <boo1ean0807@gmail.com>
2018-03-30 19:48:46 +0300

    Clean up nvm installation

--- setup.sh
+++ setup.sh
@@ -51,9 +51,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # Install node version manager
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
 
 chown $SUDO_USER:$SUDO_USER -R ./
 
